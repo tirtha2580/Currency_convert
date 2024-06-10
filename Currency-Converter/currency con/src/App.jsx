630af03dfbfbfa35efdbe221d2b4b10d512765d1
Tirthamoy Biswas <tirthamoybiswas405@gmail.com>
2024-06-11 00:53:41 +0530

    Currency Calculator-app-react

---
+++ Currency-Converter/currency con/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Currency from './Components/Currency'
+
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className='container'></div>
+   <Currency/>
+ 
+    </div>
+  )
+}
+
+export default App
